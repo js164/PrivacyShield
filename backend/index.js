62fc8ecc378d3eee2560a25ce928a3d4a2211b9e
Jay Manharbhai Solanki <jaymanharbhai.solanki@gwdg.de>
2025-07-22 23:55:30 +0200

    backend structure added

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const express=require('express')
+const app=express()
+const dotenv=require('dotenv').config()
+const mongoose=require('mongoose')
+const session=require('express-session')
+const MongoStore=require('connect-mongo')
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));
+
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store:MongoStore.create({
+        mongoUrl: process.env.MONGO_URI
+    })
+  }))
+
+
+app.use('/',require('./routes/assesment'))
+
+port=process.env.PORT || 8000
+const connectDB=require('./db')
+connectDB()
+
+
+app.listen(port,
+    console.log(`server started on port ${port}`)
+)
---
+++ backend/routes/assesment.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const route = express.Router()
+
+route.post('/report', async function(req, res, next){
+    
+})
+
+
+
+module.exports = route;
