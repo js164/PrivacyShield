c532f1b7c4e53c889e9bfe2d16d242e2348236ec
Nirish <nirish2407@gmail.com>
2025-07-23 14:36:01 +0200

    Add Navbar

--- frontend/privacyshield/src/App.jsx
+++ frontend/privacyshield/src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from "./components/HomePage"
 import Assesment from "./components/Assesment"
 import Report from "./components/Report"
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
+      <Navbar />
+      <br></br><br></br><br></br><br></br>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/assesment" element={<Assesment />} />
