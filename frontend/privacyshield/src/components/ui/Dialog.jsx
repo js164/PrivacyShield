e93232dfa84899631731effcc7a4034427d22ab5
Nirish <nirish2407@gmail.com>
2025-09-09 19:03:53 +0200

    Question Update

--- frontend/privacyshield/src/components/ui/Dialog.jsx
+++ frontend/privacyshield/src/components/ui/Dialog.jsx
@@ -7,9 +7,14 @@ export default function ContinueModal({ show, onContinue, onStop, message }) {
   return (
     <div className="modal-backdrop">
       <div className="modal-container">
+        {/*
         <h2>Almost There!</h2>
         <p>{message || "Do you want to continue with the survey to get a detailed Assessment Report?"}</p>
-        <div className="modal-buttons">
+        */}
+        <h2>Almost There! Select</h2>
+        <span className="modal-message"><strong>Yes:</strong> To keep going to get a more precise and accurate report tailored specifically to you!</span><br />
+        <span className="modal-message"><strong>No:</strong> To generate a report now based on your progress so far.</span>
+        <div className="modal-buttons mt-8">
           <button className="btn-stop" onClick={onStop}>
             No
           </button>
