866d998c057868ba77f93ae7d4ccd849a88d2868
Jay Solanki <jaysolanki164@gmail.com>
2025-09-10 10:53:26 +0200

    initial question ui fix

--- frontend/privacyshield/src/components/InitialAssesment.jsx
+++ frontend/privacyshield/src/components/InitialAssesment.jsx
@@ -31,51 +31,57 @@ export default function QuestionPage() {
 
   return (
     <>
-    <Navbar_Questions isChecking={isChecking} />
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-6 animate-fade-in-up">
-      <div className="w-full max-w-6xl bg-white rounded-3xl shadow-xl p-8 border border-gray-100">
-        <h1 className="text-2xl font-semibold mb-12 text-center">
-          {question}
-        </h1>
+      <Navbar_Questions isChecking={isChecking} />
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-6 animate-fade-in-up">
+        <div className="w-full max-w-6xl bg-white rounded-3xl shadow-xl p-8 border border-gray-100">
+          <h1 className="text-2xl font-semibold mb-12 text-center">
+            {question}
+          </h1>
 
-        <div className="grid gap-4">
-          {options.map((opt) => (
-            <div
-              key={opt.id}
-              onClick={() => toggleSelect(opt.text)}
-              className={`flex items-center gap-3 cursor-pointer rounded-2xl shadow-md transition transform hover:scale-[1.02] border-2 p-5 text-left font-semibold text-base ${
-                selected.includes(opt.text)
-                  ? "border-blue-300 bg-blue-50"
-                  : "border-gray-200 bg-white text-gray-700 hover:border-blue-300 hover:bg-blue-50"
-              }`}
+          <div className="grid gap-4">
+            {options.map((opt) => (
+              <div
+                key={opt.id}
+                onClick={() => toggleSelect(opt.text)}
+                className={`flex items-center gap-3 cursor-pointer rounded-2xl shadow-md transition transform hover:scale-[1.02] border-2 p-5 text-left font-semibold text-base ${selected.includes(opt.text)
+                    ? "border-blue-300 bg-blue-50"
+                    : "border-gray-200 bg-white text-gray-700 hover:border-blue-300 hover:bg-blue-50"
+                  }`}
+              >
+                <input
+                  type="checkbox"
+                  checked={selected.includes(opt.text)}
+                  onChange={() => toggleSelect(opt.text)}
+                  className="w-4 h-4 text-blue-500 border-gray-300 rounded cursor-pointer"
+                  onClick={(e) => e.stopPropagation()} // prevent double toggle
+                />
+                <span>{opt.text}</span>
+                <span
+                  className="text-xs font-semibold py-1 px-3 rounded-full bg-blue-100 text-blue-700 ml-auto"
+                  title={QUESTION_CATEGORIES[opt.category]}
+                >
+                  {QUESTION_CATEGORIES[opt.category]}
+                </span>
+              </div>
+            ))}
+          </div>
+
+
+
+          <div className="mt-8 flex justify-center">
+            <button
+              disabled={selected.length === 0}
+              onClick={handleSubmit}
+              className={`rounded-2xl px-8 py-3 text-lg font-bold shadow-lg transition-all duration-300 ${selected.length > 0
+                  ? "bg-blue-400 text-white rounded-lg hover:bg-primary-blue disabled:opacity-50"
+                  : "bg-gray-300 text-gray-500 cursor-not-allowed"
+                }`}
             >
-            <input
-            type="checkbox"
-            checked={selected.includes(opt.text)}
-            onChange={() => toggleSelect(opt.text)}
-            className="w-4 h-4 text-blue-500 border-gray-300 rounded cursor-pointer"
-            onClick={(e) => e.stopPropagation()} // prevent double toggle
-            />
-              {opt.text}<span className="text-gray-500 font-normal">({QUESTION_CATEGORIES[opt.category]})</span>
-            </div>
-          ))}
-        </div>
-
-        <div className="mt-8 flex justify-center">
-          <button
-            disabled={selected.length === 0}
-            onClick={handleSubmit}
-            className={`rounded-2xl px-8 py-3 text-lg font-bold shadow-lg transition-all duration-300 ${
-              selected.length > 0
-                ? "bg-blue-400 text-white rounded-lg hover:bg-primary-blue disabled:opacity-50"
-                : "bg-gray-300 text-gray-500 cursor-not-allowed"
-            }`}
-          >
-            Next
-          </button>
+              Next
+            </button>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
